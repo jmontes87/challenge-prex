d32ed229c1f8e3d43a15a54a31620d347f9cdd70
Jose Manuel Montes <jose.montes@greelow.com>
2024-05-14 19:40:01 -0300

    initial commit - config laravel passport

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $user->createToken('authToken')->plainTextToken
+        ]);
+
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Carbon;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
+        Passport::refreshTokensExpireIn(Carbon::now()->addMinutes(30));
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addMonths(6));
+    }
+}
